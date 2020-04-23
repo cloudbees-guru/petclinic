e0c90c81f07d5fbee20974b55505757402bacbca
skures <skures@cloudbees.com>
2020-04-23 10:01:25 +0200

    Create Flags.java

---
+++ src/main/java/org/cloudbees/rollout/Flags.java
@@ -0,0 +1,9 @@
+import io.rollout.flags.RoxFlag;
+import io.rollout.configuration.RoxContainer;
+
+// Create Roxflags in the Flags container class
+public class Flags implements RoxContainer {
+  // Define the feature flags
+  public RoxFlag enableTutorial = new RoxFlag(false);
+  public RoxVariant titleColors = new RoxVariant("White", new String[] {"White", "Blue", "Green"});
+}
