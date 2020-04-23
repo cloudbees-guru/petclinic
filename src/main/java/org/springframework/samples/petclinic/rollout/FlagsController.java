7bb9a2095c59b37c51cb3ace42965a8de1f89c8a
Jean-Philippe Briend <jeanphilippe.briend@gmail.com>
2020-04-23 15:25:02 +0200

    Add Roullout feature flags

---
+++ src/main/java/org/springframework/samples/petclinic/rollout/FlagsController.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.rollout;
+
+import io.rollout.configuration.RoxContainer;
+import io.rollout.flags.RoxFlag;
+import io.rollout.rox.server.Rox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.samples.petclinic.system.WelcomeController;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.ExecutionException;
+
+@Controller
+public class FlagsController implements RoxContainer {
+
+	private final static Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+	@Value("${ffkey}")
+	private String ffkey;
+
+	public RoxFlag enableFeatureOne = new RoxFlag(true);
+
+	@PostConstruct
+	void postConstruct() {
+		logger.info("Initializing Rollout flags...");
+		Rox.register("default", this);
+		try {
+			Rox.setup(ffkey).get();
+			Rox.fetch();
+		}
+		catch (InterruptedException ie) {
+			logger.error("Exception while initializing Roollout flags: ", ie);
+		}
+		catch (ExecutionException ee) {
+			logger.error("Exception while initializing Roollout flags: ", ee);
+		}
+	}
+
+}
--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -16,21 +16,12 @@
 
 package org.springframework.samples.petclinic.system;
 
-import org.cloudbees.rollout.Flags;
-import io.rollout.flags.RoxFlag;
-import io.rollout.configuration.RoxContainer;
-
-import io.rollout.client.ConfigurationFetchedHandler;
-import io.rollout.client.FetcherResults;
-import io.rollout.flags.RoxVariant;
-import io.rollout.rox.server.Rox;
-import io.rollout.rox.server.RoxOptions;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.rollout.FlagsController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 
@@ -39,27 +30,14 @@ public class WelcomeController {
 
 	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
-	@Value("${ffkey}")
-	private String ffkey;
+	@Autowired
+	private FlagsController flags;
 
 	@GetMapping("/")
 	public String welcome() throws IOException {
-		// logger.info(Flags.conf.enableFeatureOne.toString());
-
-		Flags flags = new Flags();
-
-		// Register the flags container with Rollout
-		Rox.register("", flags);
-
-		// Setup the Rollout environment key
-		Rox.setup(ffkey);
-
-		// Boolean flag example
-		if (flags.enableTutorial.isEnabled()) {
-			// TODO: Put your code here that needs to be gated
-		}
 
 		if (flags.enableFeatureOne.isEnabled()) {
+			logger.info("featureone true");
 			return "welcome_featureone";
 		}
 		else {
