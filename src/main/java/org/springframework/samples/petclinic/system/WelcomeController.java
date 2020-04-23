6ae9c30e30680e7adfe683c73a18efe073458c3b
skures <skures@cloudbees.com>
2020-04-23 13:37:37 +0200

    properties

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -21,6 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 
@@ -28,6 +29,9 @@ import java.io.IOException;
 public class WelcomeController {
 
 	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+	@Value("${ffkey}")
+	private String ffkey;
 
 	@GetMapping("/")
 	public String welcome() throws IOException {
