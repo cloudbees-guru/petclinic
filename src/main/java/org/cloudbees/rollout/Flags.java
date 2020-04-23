4898572a8018e097e52e4e99e74e07f2b01cbf1c
skures <skures@cloudbees.com>
2020-04-23 11:13:00 +0200

    Update Flags.java

--- src/main/java/org/cloudbees/rollout/Flags.java
+++ src/main/java/org/cloudbees/rollout/Flags.java
@@ -1,4 +1,4 @@
-package org.springframework.samples.petclinic;
+package com.cloudbees.rollout;
 
 import io.rollout.flags.RoxFlag;
 import io.rollout.configuration.RoxContainer;
@@ -10,7 +10,7 @@ import io.rollout.rox.server.Rox;
 import io.rollout.rox.server.RoxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.samples.petclinic.system.WelcomeController;
+
 
 // Create Roxflags in the Flags container class
 public class Flags implements RoxContainer {
