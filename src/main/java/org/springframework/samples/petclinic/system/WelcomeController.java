e4f1a58c1c4c3c49353b7816bc2c99c148d72f81
skures <40989190+skures@users.noreply.github.com>
2020-04-23 09:18:22 +0200

    Update WelcomeController.java

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -16,46 +16,15 @@
 
 package org.springframework.samples.petclinic.system;
 
-import com.cloudbees.rollout.Flags;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.IOException;
-
 @Controller
-public class WelcomeController {
-
-	Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+class WelcomeController {
 
 	@GetMapping("/")
-	public String welcome() throws IOException {
-        
-        // Define the feature flags
-        public RoxFlag enableFeatureOne = new RoxFlag(false);
-        public RoxVariant titleColors = new RoxVariant("White", new String[] { "White", "Blue", "Green" });
-        //--END  Flags
-
-
-
-        private Logger logger = LoggerFactory.getLogger(FlagsContainer.class);
-        public static FlagsContainer conf = null;
-        static {
-            //innit FlagsContainer
-            if (conf == null) {
-                try {
-                    conf = new Flags();
-                    Rox.register("default", conf);
-                    Rox.setup("5ea08b64b9e3830db8ff688a").get();
-                    logger("rollout init:"
-                     conf.toString());
-                }
-                catch (Exception e) {
-                      e.printStackTrace();
-                }
-            }
-
+	public String welcome() {
+		return "welcome";
 	}
 
 }
